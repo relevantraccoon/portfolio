f3fd23d97e0b13be4ed2226828e34de058621975
Joakim Karlsson <joakim.t.karlsson.1985@gmail.com>
2025-09-13 12:15:43 +0200

    Remove React import to Storybook preview.tsx

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,7 @@
-import type { Preview } from '@storybook/react-vite'
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from '../src/styles/theme/defaultTheme'
-import { GlobalStyles } from '../src/styles/GlobalStyles'
+import type { Preview } from "@storybook/react-vite";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "../src/styles/theme/defaultTheme";
+import { GlobalStyles } from "../src/styles/GlobalStyles";
 
 const preview: Preview = {
   decorators: [
@@ -16,8 +15,8 @@ const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
@@ -25,8 +24,8 @@ const preview: Preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+      test: "todo",
+    },
   },
 };
 
