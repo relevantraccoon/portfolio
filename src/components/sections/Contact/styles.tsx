cd0ac27f345ca41d0f258582c7a6028b3025864a
Joakim Karlsson <33528887+relevantraccoon@users.noreply.github.com>
2026-04-17 17:25:12 +0200

    Switches viewport units from vh to svh/dvh for mobile browser UI (#78)

--- src/components/sections/Contact/styles.tsx
+++ src/components/sections/Contact/styles.tsx
@@ -3,7 +3,7 @@ import { mediaQuery } from "@/styles/theme/mediaQueries";
 
 export const ContactWrapper = styled.section`
   width: 100%;
-  height: 100vh;
+  height: 100dvh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -21,8 +21,8 @@ export const ContactContainer = styled.div`
   width: 100%;
   height: auto;
   min-height: 500px;
-  max-height: 80vh;
-  overflow: hidden;
+  max-height: 80dvh;
+  overflow-y: auto;
   text-align: center;
   position: relative;
   display: flex;
@@ -44,7 +44,7 @@ export const ContactContainer = styled.div`
 
   @media (pointer: coarse) and (max-height: 500px) {
     min-height: 0;
-    max-height: 100svh;
+    max-height: 100dvh;
   }
 `;
 
